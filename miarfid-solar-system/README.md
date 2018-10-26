59f568159bc8ad1957447155843e2615ac91f7f2
Tim Joug <timothe.jouglet@isen.yncrea.fr>
2018-10-26 11:30:46 +0200

    Add first iteraation project

---
+++ miarfid-solar-system/README.md
@@ -0,0 +1,5 @@
+# miarfid-solar-system
+
+Solar system built using the library Three.js for the computer graphics subject from MIARFID.
+
+A working example is hosted [here](http://lvapeab.github.io/solar_system_demo.htm)
---
+++ scene.js
@@ -0,0 +1,42 @@
+var renderer, scene, camera, mesh;
+
+init();
+animate();
+
+function init(){
+    // on initialise le moteur de rendu
+    renderer = new THREE.WebGLRenderer();
+
+    // si WebGL ne fonctionne pas sur votre navigateur vous pouvez utiliser le moteur de rendu Canvas à la place
+    // renderer = new THREE.CanvasRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById('container').appendChild(renderer.domElement);
+
+    // on initialise la scène
+    scene = new THREE.Scene();
+
+    // on initialise la camera que l’on place ensuite sur la scène
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.set(0, 0, 1000);
+    scene.add(camera);
+    
+    // on créé un  cube au quel on définie un matériau puis on l’ajoute à la scène 
+    var geometry = new THREE.SphereGeometry( 200, 15, 15 );
+    var material = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture('src/test.jpg'), overdraw: true } );
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    // on ajoute une lumière blanche
+    var lumiere = new THREE.DirectionalLight( 0xffffff, 1.0 );
+    lumiere.position.set( 0, 0, 400 );
+    scene.add( lumiere );
+
+    // on effectue le rendu de la scène
+    renderer.render( scene, camera );
+}
+
+function animate(){
+    requestAnimationFrame( animate );
+    mesh.rotation.y += 0.10;
+    renderer.render( scene, camera );
+}
